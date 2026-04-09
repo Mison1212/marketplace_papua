d2683e49ebeff12d80f1f14006120005084257b1
Mison1212 <misonwenda20@gmail.com>
2026-04-10 02:10:31 +0700

    implementasi Firebase Notification

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -4,14 +4,11 @@ class NotificationService {
   final FirebaseMessaging _fcm = FirebaseMessaging.instance;
 
   Future<void> initNotification() async {
-    // Meminta izin notifikasi (penting untuk iOS/Android 13+)
     await _fcm.requestPermission();
 
-    // Mendapatkan token untuk testing (opsional)
     String? token = await _fcm.getToken();
     print("Firebase Token: $token");
 
-    // Menangani notifikasi saat aplikasi di foreground
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       print("Pesan diterima: ${message.notification?.title}");
     });
