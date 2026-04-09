9ebdaabb3809cadf427bedc6eed794c779822114
Mison1212 <misonwenda20@gmail.com>
2026-04-10 02:43:23 +0700

    firebase-message-sw.js dan goole-services.json

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,13 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-firebase.initializeApp({
-  apiKey: "API_KEY_KAMU",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
-});
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCx-WJZxA08S04Cx1EzIyHtuoCagOP-tw4",
+  authDomain: "uts-marketplace.firebaseapp.com",
+  projectId: "uts-marketplace",
+  storageBucket: "uts-marketplace.firebasestorage.app",
+  messagingSenderId: "814252788949",
+  appId: "1:814252788949:web:e53d0a7eb41cf5e0c2265c",
+  measurementId: "G-2CCJS1E408"
+};
 
-const messaging = firebase.messaging();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
