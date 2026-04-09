ef23e6324007bfbe43dee303232f690c1334e61e
Mison1212 <misonwenda20@gmail.com>
2026-04-10 02:32:47 +0700

    firebase-message-sw.js dan goole-services.json

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "API_KEY_KAMU",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+});
+
+const messaging = firebase.messaging();
