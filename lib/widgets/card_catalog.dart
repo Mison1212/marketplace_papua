acad218c2af55c03bcd4681bc4325229fb0a8322
Mison1212 <misonwenda20@gmail.com>
2026-04-15 19:39:30 +0700

    feat(katalog): implementasi widget CardCatalog sebagai komponen reusable
    - Membuat StatelessWidget CardCatalog dengan parameter Product
    - Menampilkan gambar produk dengan ClipRRect dan border radius
    - Menambahkan fallback icon jika gambar gagal dimuat
    - Menampilkan nama produk dengan batas 1 baris (ellipsis)
    - Menampilkan deskripsi produk dengan batas 2 baris
    - Menampilkan harga dengan format sederhana (Rp)
    - Mengatur tampilan menggunakan Card dengan elevation dan styling

--- lib/widgets/card_catalog.dart
+++ lib/widgets/card_catalog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../models/product_model.dart';
+import 'package:marketplace_papua/models/product_model.dart';
 
 class CardCatalog extends StatelessWidget {
   final Product product;
